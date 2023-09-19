1308115dfd5d750fa14499ef1bd4d6814e38dc40
Your Name <you@example.com>
2023-09-19 20:12:10 -0300

    reentrega2

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import cart from './assets/cart.svg';
+
+const CartWidget = () => { 
+    return (
+        <div>
+            <img src={cart} width="30" heigth="30"  alt="cart-widget"/>
+            0
+        </div>
+    )
+}
+export default CartWidget
